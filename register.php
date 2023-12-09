8d4f6b2447ea5cce4dc80b3988784505ccd81078
F-Shapiro <abstractxunxon@gmail.com>
2023-12-09 14:56:02 +0300

    minor fix db connection

--- register.php
+++ register.php
@@ -9,8 +9,6 @@ $conn = new mysqli(
     $dbParams['database']
 );
 
-$conn = new mysqli($servername, $username, $password, $database);
-
 if ($conn->connect_error) {
     die("Ошибка подключения к базе данных: " . $conn->connect_error);
 }
