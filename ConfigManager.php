a84b2fe8ef5f10f6578e923b91855faefdf4609f
F-Shapiro <abstractxunxon@gmail.com>
2023-12-09 22:14:00 +0300

    add ConfigManager

---
+++ ConfigManager.php
@@ -0,0 +1,21 @@
+<?php
+
+class ConfigManager
+{
+    protected $env;
+
+    public function __construct()
+    {
+        $this->env = parse_ini_file('.env');
+    }
+
+    public function getDBParam()
+    {
+        return [
+            'servername' => $this->env['DB_HOST'],
+            'username' => $this->env['DB_USERNAME'],
+            'password' => $this->env['DB_PASSWORD'],
+            'database' => $this->env['DB_DATABASE'],
+        ];
+    }
+}
