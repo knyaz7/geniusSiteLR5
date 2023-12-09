250f26a4d6579237ea2175db4a6e01077ae415e3
F-Shapiro <abstractxunxon@gmail.com>
2023-12-09 16:32:40 +0300

    User add getter and setter

--- User.php
+++ User.php
@@ -9,4 +9,39 @@ class User
         $this->password = $password;
         $this->email = $email;
     }
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    public function getPasswrord()
+    {
+        return $this->password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function showAttr()
+    {
+        return "Логин: {$this->login}\nПароль: {$this->password}\nE-mail: {$this->email}";
+    }
 }
